0a506bd420eb5ad5af2034ccdecf1105ef6ace53
jonn <jonn@jonn.nu>
2020-11-18 21:50:09 +0100

    fix: update entity with abstract create

--- src/entity/EntityBase.ts
+++ src/entity/EntityBase.ts
@@ -20,34 +20,26 @@ export interface IEntityBaseOptions {
 export interface IEntityEvent {
   name: string;
   payload: TObject<any>;
-  created: Date;
+  date: Date;
 }
 
 export abstract class EntityBase implements IEntity {
   protected _id: string;
-  protected _version: number;
   protected _created: Date;
   protected _updated: Date;
   protected _events: Array<IEntityEvent>;
+  protected _version: number;
 
   protected constructor(options: IEntityBaseOptions = {}) {
     this._id = options.id || uuid();
-    this._version = options.version || 0;
     this._created = options.created || new Date();
     this._updated = options.updated || new Date();
     this._events = options.events || [];
+    this._version = options.version || 0;
   }
 
   public get id(): string {
     return this._id;
-  }
-
-  public get version(): number {
-    return this._version;
-  }
-
-  public set version(version: number) {
-    this._version = version;
   }
 
   public get created(): Date {
@@ -57,7 +49,6 @@ export abstract class EntityBase implements IEntity {
   public get updated(): Date {
     return this._updated;
   }
-
   public set updated(updated: Date) {
     this._updated = updated;
   }
@@ -66,11 +57,20 @@ export abstract class EntityBase implements IEntity {
     return this._events;
   }
 
+  public get version(): number {
+    return this._version;
+  }
+  public set version(version: number) {
+    this._version = version;
+  }
+
+  protected abstract create(): void;
+
   protected addEvent(name: string, payload: TObject<any>): void {
     this._events.push({
       name,
       payload,
-      created: new Date(),
+      date: new Date(),
     });
   }
 }
