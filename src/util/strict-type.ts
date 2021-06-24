90b179fff239e24ae6ab85f5794551ff2bb09848
Jonn Nilsson <jonn@jonn.nu>
2021-06-24 14:01:20 +0200

    fix: update typing on strict type utilities

--- src/util/strict-type.ts
+++ src/util/strict-type.ts
@@ -1,9 +1,9 @@
-import { isArray, isDate, isError, isObject, isString } from "lodash";
+import { isArray, isDate, isError, isObject, isString, isUndefined } from "lodash";
 
-export const isArrayStrict = (input: unknown): boolean => {
-  return isArray(input) && !isString(input);
+export const isArrayStrict = (input: unknown): input is Array<unknown> => {
+  return isArray(input) && !isString(input) && !isUndefined(input);
 };
 
-export const isObjectStrict = (input: unknown): boolean => {
+export const isObjectStrict = (input: unknown): input is Record<string, unknown> => {
   return isObject(input) && !isArray(input) && !isDate(input) && !isError(input);
 };
